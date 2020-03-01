90f8cb1ea674b6cad8117e3507bed2ffa17c251f
goli <bhargavgoli@gmail.com>
2020-03-01 13:00:06 +0530

    payment details

--- appsync/paymentIntent.js
+++ appsync/paymentIntent.js
@@ -11,17 +11,40 @@ const SECERT_KEY = "sk_test_PkuVCtuwhPb99IN1XFaxCBrm00BLaemmP4";
 const stripe = require('stripe')(SECERT_KEY);
 
 module.exports.handler = async event => {
-    const {amount} = event.arguments;
+    const {amount, cardNo, cvv, expMonth, expYear, zipCode} = event.arguments;
+    //1. create payment method
+    const paymentMethod = await stripe.paymentMethods.create({
+        type: 'card',
+        card: {
+            number: cardNo,
+            exp_month: expMonth,
+            exp_year: expYear,
+            cvc: cvv
+        }
+    });
+    console.log("PAY-METHOD", paymentMethod);
+    const payId = paymentMethod.id;
+
+    //2. create payment intent
     const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: 'usd',
         payment_method_types: ['card'],
+        payment_method: payId,
+        confirm: true
     });
+    console.log("PAY-INTENT", paymentIntent);
+
+
+    //3. capture payment using payment intent
+    const paymentCapture = await stripe.paymentIntents.capture(paymentIntent.id);
+    console.log("PAY-CAPTURE", paymentCapture);
 
     // Send publishable key and PaymentIntent details to client
     const result = {
-        key: process.env.STRIPE_PUBLISHABLE_KEY,
-        secret: paymentIntent.client_secret
+        transId: paymentCapture.id,
+        status: paymentCapture.status,
+        amount: paymentCapture.amount
     };
 
     return result;
