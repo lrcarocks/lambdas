f0e75c555b44655aa681807bb336662725b89bd9
goli <bhargavgoli@gmail.com>
2020-03-01 11:47:37 +0530

    payment mutation added.

---
+++ appsync/paymentIntent.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const client = require('data-api-client')({
+    secretArn: process.env.AWS_SECRET_STORE_ARN,
+    resourceArn: process.env.DB_CLUSTER_ARN,
+    database: process.env.DB_NAME
+});
+
+
+module.exports.handler = async event => {
+    const {amount} = event.arguments;
+
+    return event;
+};
