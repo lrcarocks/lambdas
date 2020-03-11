27d5e40f8352e139d8fc2df6251a5db59b186d72
goli <bhargavgoli@gmail.com>
2020-03-11 11:21:40 +0530

    fixes

--- appsync/getAvailableSpots.js
+++ appsync/getAvailableSpots.js
@@ -22,9 +22,9 @@ module.exports.handler = async event => {
     console.log("ProviderId:", providerId);
 
     try {
-        const query = 'SELECT t.UserID, t.EntryTime, t.ExitTime, t.IsConfirmed, p.ID, p.ProviderID, p.WingCode, p.Latitude, p.Longitude ' +
-            'FROM ParkingSpotDetails p, ParkingTransactionDetails t ' +
-            'WHERE p.ProviderID IN (:providerId) AND t.ParkingSpotID=p.ID AND t.TrasactionCompleted=false';
+        const query = 'SELECT t.UserID, t.EntryTime, t.ExitTime, t.IsConfirmed, p.ID, p.ProviderID, p.WingCode, p.Latitude, p.Longitude, u.PhoneNumber ' +
+            'FROM ParkingSpotDetails p, ParkingTransactionDetails t, UserDetails u ' +
+            'WHERE p.ProviderID IN (:providerId) AND u.ID=t.UserID AND t.ParkingSpotID=p.ID AND t.TrasactionCompleted=false';
         let response = await client.query(query, {providerId: providerId, tStatus});
         console.log(response.records);
         return response.records;
